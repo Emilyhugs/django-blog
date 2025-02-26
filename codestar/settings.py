884240fa62d923f96629ad4c877645cf9b383c65
Emily Huggins <emilyhuggins@btinternet.com>
2025-02-26 16:31:03 +0000

    add Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -128,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://*127.0.0.1:8000/",
+    "https://*.herokuapp.com"
+]
